dd529f285da547ca5f0921d45d12bc30e3130357
jikoyster <yu.jake@gmail.com>
2022-11-08 20:56:17 -0800

    last edits for this landing page

--- index.php
+++ index.php
@@ -4,9 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/reseter.css/2.0.0/reseter.min.css" integrity="sha512-gCJkkUMGTe73+FMwog6gIBCVJIMXRoc21l6/IPCuzxCex/1sxvO8ctb6Zd4/WWs2UMqmtnDrAdhJht5pEY0LXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
     <title>Document</title>
 </head>
+
+
+<?php
+$home_url = "http://www.bestworldwideforyou.com";
+?>
+
 <body>
+    <div class="container-sm">
+        <center class="col-12">
+            <div class="d-none d-md-block class-lg">   
+                <div class="ubox sm-box topbox">
+                    <p>Something cosmic is coming your way with...</p>
+                    <a href=<?=$home_url?>>WWW.BESTWORLDWIDEFORYOU.COM</a>
+                </div>
+            </div>
+            <div class="d-block d-md-none class-sm"> 
+                <div class="ubox sm-box topbox">
+                    <p>Something cosmic is coming your way with...</p>
+                    <a href=<?=$home_url?>>WWW.BESTWORLDWIDEFORYOU.COM</a>
+                </div>
+            </div>
+        </center>
+    </div>
     
 </body>
 </html>
